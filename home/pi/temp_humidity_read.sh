ec1c0cee32b211f131045f7a862b7397bbd3c152
Perttu Taskinen <perttu.taskinen@gmail.com>
2022-07-16 12:50:06 +0300

    Use outlook.com instead on gmail to send status email

--- home/pi/temp_humidity_read.sh
+++ home/pi/temp_humidity_read.sh
@@ -1,2 +1,4 @@
-date > /tmp/temp_humidity.txt
+# extra newline for Outlook.com behavior not to forward email content without one
+echo > /tmp/temp_humidity.txt
+date >> /tmp/temp_humidity.txt
 /home/pi/git/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4 >> /tmp/temp_humidity.txt
